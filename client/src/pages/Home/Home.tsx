331359e13db687fe8217627748c3f4885c545a73
xley <42159074+xley@users.noreply.github.com>
2021-07-10 14:47:32 +1000

    Include Home Page (#1)

---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,24 @@
+function Home() {
+    return (
+        <div className="Home">
+            <header className="Home-header">
+                <h1>
+                    Sculptley
+                </h1>
+                <h2>
+                    Slogan
+                </h2>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Connect
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default Home;
