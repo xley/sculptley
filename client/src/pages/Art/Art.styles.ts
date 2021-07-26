f43134d328d60b51a8f01ec3c84d881a512020a8
Joshua Page <joshuapage247@gmail.com>
2021-07-26 14:24:43 +1000

    Compleeted Art Title, Header, sub categories and blurb

--- client/src/pages/Art/Art.styles.ts
+++ client/src/pages/Art/Art.styles.ts
@@ -2,4 +2,76 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   margin: 40px;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const SectionHeader = styled.h1`
+  font-size: 5rem;
+  font-family: sans-serif;
+  text-align: center;
+  writing-mode: tb-rl;
+  transform: rotate(-180deg);
+  margin: 1rem;
+`;
+
+export const ArtSection = styled.div`
+  border: 1px solid black;
+  padding: 1rem;
+`;
+
+export const StyledTitle = styled.h1`
+  font-size: 4.5rem;
+  font-family: sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  text-align: center;
+  margin-bottom: 5px; // Not sure if this is good practice
+`;
+
+export const StyledCompany = styled.h2`
+  font-size: 2.25rem;
+  font-family: sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  text-align: center;
+  margin-top: 0px; // Good practice?
+  margin-bottom: 10px; // As above
+`;
+
+export const StyledDetailDiv = styled.div`
+  justify-content: center;
+`;
+
+export const StyledDetail = styled.h3`
+  font-size: 1.5rem;
+  font-family: sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  display: inline-block;
+  text-align: center;
+  margin-top: 0px; // Not sure if manipulating margin is correct practice
+  margin-bottom: 10px; // As above
+  width: 190px; // Copied width from Figma (not sure if you wanted exact)
+  height: 40px; // As above
+`;
+
+export const StyledBlurb = styled.p`
+  font-size: 1.5rem;
+  font-family: sans-serif;
+  text-align: justify;
+  border-left: 1px solid black;
+  margin: auto;
+  padding: 0 1rem;
+  width: 40%;
+  min-width: 300px;
+`;
+
+export const StyledButton = styled.button`
+  font-size: 1.5rem;
+  float: right;
+  margin: 1rem;
+  border: none;
+  border-bottom: 1px solid black;
+  cursor: pointer;
 `;
--- client/src/pages/Art/Art.tsx
+++ client/src/pages/Art/Art.tsx
@@ -1,9 +1,25 @@
-import { Wrapper } from "./Art.styles";
+import { ArtSection, SectionHeader, StyledBlurb, StyledButton, StyledCompany, StyledDetail, StyledDetailDiv, StyledTitle, Wrapper } from "./Art.styles";
 
 function Art() {
     return (
         <Wrapper>
-            Art
+            <SectionHeader>ART</SectionHeader>
+            <ArtSection>
+                <StyledTitle>TITLE</StyledTitle>
+                <StyledCompany>COMPANY</StyledCompany>
+                <StyledDetailDiv>
+                    <StyledDetail>DATE</StyledDetail>
+                    <StyledDetail>LOCATION</StyledDetail>
+                </StyledDetailDiv>
+                <StyledBlurb>
+                    Blurb detailing the art and the
+                    story behind the piece. Blurb 
+                    detailing the art and the story 
+                    behind the piece. Blurb detailing 
+                    the art and the story..
+                </StyledBlurb>
+                <StyledButton>VIEW ART</StyledButton> {/* Missing > icon, I figure &gt; isn't what is desired*/}
+            </ArtSection>
         </Wrapper>
     );
 }
