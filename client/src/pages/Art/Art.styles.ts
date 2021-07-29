27c6278e955418a02ede9a56b1215706fb5e7e69
Josh <72534526+1BAN@users.noreply.github.com>
2021-07-29 18:24:31 +1000

    Completed Art Title, Header, sub categories and blurb (#7)
    * Compleeted Art Title, Header, sub categories and blurb
    
    * Removed pixel values, added ArtSection width
    
    * Fix styling changes and button
    
    Co-authored-by: xley <christopher.crossley@intellihr.co>

--- client/src/pages/Art/Art.styles.ts
+++ client/src/pages/Art/Art.styles.ts
@@ -1,5 +1,89 @@
 import styled from "styled-components";
+import { ArrowRightS } from "@styled-icons/remix-line/ArrowRightS";
+
+export const ArrowIcon = styled(ArrowRightS)`
+  color: black;
+  height: 4vh;
+  border-bottom: 0;
+`;
 
 export const Wrapper = styled.div`
   margin: 2rem;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const SectionHeader = styled.h1`
+  font-size: 5rem;
+  font-family: sans-serif;
+  text-align: center;
+  writing-mode: tb-rl;
+  transform: rotate(-180deg);
+  margin: 1rem;
+`;
+
+export const ArtSection = styled.div`
+  border: 1px solid black;
+  padding: 1rem;
+  width: 100%;
+  min-width: 300px;
+`;
+
+export const StyledTitle = styled.h1`
+  font-size: 4.5rem;
+  font-family: sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  text-align: center;
+  margin-bottom: 0.5rem;
+`;
+
+export const StyledCompany = styled.h2`
+  font-size: 2.25rem;
+  font-family: sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  text-align: center;
+  margin: 0 auto 0.75rem auto;
+`;
+
+export const StyledDetailDiv = styled.div`
+  justify-content: center;
+`;
+
+export const StyledDetail = styled.h3`
+  font-size: 1.5rem;
+  font-family: sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  display: inline-block;
+  text-align: center;
+  margin: 1rem;
+  width: 15%;
+`;
+
+export const StyledBlurb = styled.p`
+  font-size: 1.5rem;
+  font-family: sans-serif;
+  text-align: justify;
+  border-left: 1px solid black;
+  margin: auto;
+  padding: 0 1rem;
+  width: 40%;
+  min-width: 300px;
+`;
+
+export const StyledButton = styled.button`
+  display: flex;
+  align-items: center;
+  float: right;
+  margin: 1rem;
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
+`;
+
+export const StyledButtonText = styled.p`
+  border-bottom: 1px solid black;
+  font-size: 1.5rem;
 `;
--- client/src/pages/Art/Art.tsx
+++ client/src/pages/Art/Art.tsx
@@ -1,11 +1,39 @@
-import { Wrapper } from "./Art.styles";
+import {
+  ArrowIcon,
+  ArtSection,
+  SectionHeader,
+  StyledBlurb,
+  StyledButton,
+  StyledButtonText,
+  StyledCompany,
+  StyledDetail,
+  StyledDetailDiv,
+  StyledTitle,
+  Wrapper,
+} from "./Art.styles";
 
 function Art() {
-    return (
-        <Wrapper>
-            Art
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <SectionHeader>ART</SectionHeader>
+      <ArtSection>
+        <StyledTitle>TITLE</StyledTitle>
+        <StyledCompany>COMPANY</StyledCompany>
+        <StyledDetailDiv>
+          <StyledDetail>DATE</StyledDetail>
+          <StyledDetail>LOCATION</StyledDetail>
+        </StyledDetailDiv>
+        <StyledBlurb>
+          Blurb detailing the art and the story behind the piece. Blurb
+          detailing the art and the story behind the piece. Blurb detailing the
+          art and the story..
+        </StyledBlurb>
+        <StyledButton>
+          <StyledButtonText>VIEW ART</StyledButtonText> <ArrowIcon />
+        </StyledButton>
+      </ArtSection>
+    </Wrapper>
+  );
 }
 
 export default Art;
