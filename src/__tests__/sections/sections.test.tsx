f8d637abe37657d049863acd152329e6994d43fb
xley <42159074+xley@users.noreply.github.com>
2021-08-14 12:52:17 +1000

    Include pages and sections tests for QA (#24)
    Co-authored-by: xley <christopher.crossley@intellihr.co>

---
+++ src/__tests__/sections/sections.test.tsx
@@ -0,0 +1,69 @@
+import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../../theme";
+import Landing from "../../sections/Landing/Landing";
+import About from "../../sections/About/About";
+import Art from "../../sections/Art/Art";
+import Connect from "../../sections/Connect/Connect";
+
+it("renders landing section", () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <Landing scrollTo={() => console.log("Scroll To Connect")} />
+    </ThemeProvider>
+  );
+
+  expect(screen.getByText("BRIAN")).toBeInTheDocument();
+  expect(screen.getByText("CROSSLEY")).toBeInTheDocument();
+  expect(screen.getByText("ARTIST | SCULPTOR | CREATOR")).toBeInTheDocument();
+  expect(screen.getByText("CONNECT")).toBeInTheDocument();
+});
+
+it("renders about section", () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <About scrollTo={() => console.log("Scroll To Connect")} />
+    </ThemeProvider>
+  );
+
+  expect(screen.getByText("ABOUT")).toBeInTheDocument();
+  expect(screen.getByText("briancrossley")).toBeInTheDocument();
+  expect(
+    screen.getByText(
+      "An artist with years of industry and professional experience, specializing in scenic artistry and sculpting. Taking on projects for movies, bars, clubs, zoo's and much more, Brian Crossley is a well-known name in the industry. Brian enjoys challenging his ability to capture the scenic vision whilst empowering the art with realistic intent."
+    )
+  ).toBeInTheDocument();
+  expect(screen.getByText("CONNECT")).toBeInTheDocument();
+});
+
+it("renders art section", () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <Art />
+    </ThemeProvider>
+  );
+  expect(screen.getByText("ART")).toBeInTheDocument();
+  expect(screen.getByText("TITLE")).toBeInTheDocument();
+  expect(screen.getByText("COMPANY")).toBeInTheDocument();
+  expect(screen.getByText("DATE")).toBeInTheDocument();
+  expect(screen.getByText("LOCATION")).toBeInTheDocument();
+  expect(
+    screen.getByText(
+      "Blurb detailing the art and the story behind the piece. Blurb detailing the art and the story behind the piece. Blurb detailing the art and the story.."
+    )
+  ).toBeInTheDocument();
+  expect(screen.getByText("VIEW ART")).toBeInTheDocument();
+});
+
+it("renders connect section", () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <Connect connectRef={null} />
+    </ThemeProvider>
+  );
+
+  expect(screen.getByText("CONNECT")).toBeInTheDocument();
+  expect(screen.getByText("briancrossley.linkedin")).toBeInTheDocument();
+  expect(screen.getByText("briancrossley.facebook")).toBeInTheDocument();
+  expect(screen.getByText("SEND")).toBeInTheDocument();
+});
